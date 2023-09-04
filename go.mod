2ea778ec0f332d6728f8a39f771e4e93cdf4e982
leechanhui1234 <yepp0517@naver.com>
2023-09-04 21:29:48 +0900

    add decoding

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.19
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da // indirect
 	golang.org/x/net v0.7.0 // indirect
 )
--- goCrawler.go
+++ goCrawler.go
@@ -4,21 +4,25 @@ import (
 	"fmt"
 	"net/http"
 	// "strconv"
+	iconv "github.com/djimenez/iconv-go"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	url := "https://yepp0517.tistory.com/2"
-
+	url := "https://www.g2b.go.kr:8101/ep/tbid/tbidList.do?searchType=1&bidSearchType=1&taskClCds=5&bidNm=%BA%ED%B7%CF%C3%BC%C0%CE&searchDtType=1&fromBidDt=2018%2F2%2F01&toBidDt=2018%2F3%2F01&setMonth1=1&fromOpenBidDt=&toOpenBidDt=&radOrgan=1&instNm=&instSearchRangeType=&refNo=&area=&areaNm=&strArea=&orgArea=&industry=&industryCd=&upBudget=&downBudget=&budgetCompare=&detailPrdnmNo=&detailPrdnm=&procmntReqNo=&intbidYn=&regYn=Y&recordCountPerPage=100"
+	titles := []string{}
 	res, _ := http.Get(url)
 		doc, _ := goquery.NewDocumentFromResponse(res)
 
-		links := doc.Find(".tt_article_useless_p_margin")
+		links := doc.Find(".table_list_tbidTbl tbody tr")
 		
 		links.Each(func(i int, s *goquery.Selection) {
 			// For each item found, get the title
-			title := s.Find("#og_1627608827011").Text()
-			fmt.Printf("Review %d: %s\n", i, title)
+			title := s.Find("a").Text()
+			out, _ := iconv.ConvertString(string(title), "euc-kr", "utf-8")
+			titles = append(titles, out)
 		})
+
+		fmt.Println(titles)
 }
